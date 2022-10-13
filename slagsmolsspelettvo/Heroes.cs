63b5f85e98ef2b091b91afbabf8ee29f7ef80bfd
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2022-10-13 10:44:59 +0200

    progam

--- slagsmolsspelettvo/Heroes.cs
+++ slagsmolsspelettvo/Heroes.cs
@@ -7,8 +7,11 @@ public int hp = 10;
 public Weapon weapon;
 public string name;
 
-public void Engage(Heroes target) {
-
+public void Engage(Heroes chosen) {
+    int AD = weapon.DamageTaken();
+chosen.hp -= AD;
+chosen.hp = Math.Max(0, chosen.hp);
+Console.WriteLine($"{name} do {AD} to {chosen.name}");
 }
 }
 
--- slagsmolsspelettvo/Program.cs
+++ slagsmolsspelettvo/Program.cs
@@ -1,4 +1,32 @@
 ﻿using System;
 
-Heroes Hero = new Heroes();
+Heroes Champion = new Heroes();
+
+Champion.name = "idk";
+Champion.weapon = new Weapon();
+
+Heroes Monster = new Heroes();
+Monster.name = "rawr";
+Monster.weapon = new Weapon();
+
+
+Random generator = new Random();
+while (Champion.hp > 0 && Monster.hp > 0)
+{
+    Console.WriteLine("fight start");
+    Console.WriteLine($"{Champion.name}: {Champion.hp} || {Monster.name}: {Monster.hp}");
+
+    Champion.Engage(Monster);
+    Monster.Engage(Champion);
+    
+
+
+
+
+
+
+
+
+}
+
 
